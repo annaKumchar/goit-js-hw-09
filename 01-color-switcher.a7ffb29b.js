a5e0788156e02f29c3c09e8364809557dcbd5fa8
annaKumchar <113632878+annaKumchar@users.noreply.github.com>
2022-11-16 08:11:08 +0000

    Deploying to gh-pages from @ annaKumchar/goit-js-hw-09@c06cf30de0726f2d8d7f3e0fb112e2b042ff750e 🚀

---
+++ 01-color-switcher.a7ffb29b.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),o=null;function a(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}e.addEventListener("click",(function(){e.disabled=!0,n.disabled=!1,o=setInterval(a,1e3)})),n.addEventListener("click",(function(){e.disabled=!1,n.disabled=!0,clearInterval(o)}))}();
+//# sourceMappingURL=01-color-switcher.a7ffb29b.js.map
